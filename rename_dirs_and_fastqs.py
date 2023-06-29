07e7fa4a9209b08d55c196491487f3a6eac8b66b
Daniel Gaston <dan.gaston@nshealth.ca>
2023-06-29 13:52:15 -0300

    Testing renaming directories

--- rename_dirs_and_fastqs.py
+++ rename_dirs_and_fastqs.py
@@ -11,6 +11,8 @@ if __name__ == "__main__":
     for file in os.listdir(rootdir):
         d = os.path.join(rootdir, file)
         if os.path.isdir(d):
+            elements = d.split('.')
+            sys.stdout.write(f"Renaming Directory {d} to {elements[-1]}")
             os.chdir(d)
             fastqs = glob.glob('*_R1.fastq.gz')
             for fastq1 in fastqs:
@@ -24,11 +26,15 @@ if __name__ == "__main__":
                 index1 = f"{elements3[0]}_I1.fastq.gz"
                 index2 = f"{elements3[0]}_I2.fastq.gz"
 
-                sys.stdout.write(f"Renaming {fastq1} to {sample_id}.R1.fastq.gz\n")
+                #sys.stdout.write(f"Renaming {fastq1} to {sample_id}.R1.fastq.gz\n")
                 #os.rename(fastq1, f"{sample_id}.R1.fastq.gz")
 
-                sys.stdout.write(f"Renaming {fastq2} to {sample_id}.R2.fastq.gz\n")
+                #sys.stdout.write(f"Renaming {fastq2} to {sample_id}.R2.fastq.gz\n")
+                #os.rename(fastq2, f"{sample_id}.R2.fastq.gz")
 
-                sys.stdout.write(f"Renaming {index1} to {sample_id}.I1.fastq.gz\n")
+                #sys.stdout.write(f"Renaming {index1} to {sample_id}.I1.fastq.gz\n")
+                #os.rename(index1, f"{sample_id}.I1.fastq.gz")
 
-                sys.stdout.write(f"Renaming {index2} to {sample_id}.I2.fastq.gz\n")
+                #sys.stdout.write(f"Renaming {index2} to {sample_id}.I2.fastq.gz\n")
+                #os.rename(index2, f"{sample_id}.I2.fastq.gz")
+            os.chdir(rootdir)
