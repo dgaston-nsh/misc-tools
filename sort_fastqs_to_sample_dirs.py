d05d0fab861d21114f186d2504322f5239f05c05
Daniel Gaston <dan.gaston@nshealth.ca>
2023-06-29 12:19:35 -0300

    Adding script

---
+++ sort_fastqs_to_sample_dirs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+# Standard packages
+import os
+import glob
+import shutil
+
+if __name__ == "__main__":
+    cwd = os.getcwd()
+       fastqs = glob.glob('*_R1.fastq.gz')
+       for fastq1 in fastqs:
+           elements = fastq.split('_R1')
+           sample_id = elements[0]
+           fastq2 = f"{sample_id}_R2.fastq.gz"
+           index1 = f"{sample_id}_I1.fastq.gz"
+           index2 = f"{sample_id}_I2.fastq.gz"
+
+           os.mkdir(sample_id)
+           shutil.move(fastq1, f"{sample_id}/{fastq1}")
+           shutil.move(fastq1, f"{sample_id}/{fastq2}")
+           shutil.move(fastq1, f"{sample_id}/{index1}")
+           shutil.move(fastq1, f"{sample_id}/{index2}")
