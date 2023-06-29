57a3adfcec4e6d486df8a5076e9c1a84b566f44f
Daniel Gaston <dan.gaston@nshealth.ca>
2023-06-29 12:21:48 -0300

    Fixing variable name

--- sort_fastqs_to_sample_dirs.py
+++ sort_fastqs_to_sample_dirs.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     cwd = os.getcwd()
     fastqs = glob.glob('*_R1.fastq.gz')
     for fastq1 in fastqs:
-        elements = fastq.split('_R1')
+        elements = fastq1.split('_R1')
         sample_id = elements[0]
         fastq2 = f"{sample_id}_R2.fastq.gz"
         index1 = f"{sample_id}_I1.fastq.gz"
