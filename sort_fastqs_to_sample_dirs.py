40e380ec69ff84c5f44162f077cf962941a46360
Daniel Gaston <dan.gaston@nshealth.ca>
2023-08-30 13:06:35 -0300

    Tweak settings for specific FastQs

--- sort_fastqs_to_sample_dirs.py
+++ sort_fastqs_to_sample_dirs.py
@@ -7,11 +7,11 @@ import shutil
 
 if __name__ == "__main__":
     cwd = os.getcwd()
-    fastqs = glob.glob('*_R1.fastq.gz')
+    fastqs = glob.glob('*_R1_001.fastq.gz')
     for fastq1 in fastqs:
-        elements = fastq1.split('_R1')
+        elements = fastq1.split('_R1_001')
         sample_id = elements[0]
-        fastq2 = f"{sample_id}_R2.fastq.gz"
+        fastq2 = f"{sample_id}_R2_001.fastq.gz"
         # index1 = f"{sample_id}_I1.fastq.gz"
         # index2 = f"{sample_id}_I2.fastq.gz"
 
