f18f9e6db57ba61bad47d0ff6960225826c1de27
Daniel Gaston <dan.gaston@nshealth.ca>
2023-08-31 10:55:49 -0300

    Fixing again

--- add_tag_extension_fastqs.py
+++ add_tag_extension_fastqs.py
@@ -12,14 +12,14 @@ if __name__ == "__main__":
         d = os.path.join(rootdir, file)
         if os.path.isdir(d):
             os.chdir(d)
-            fastqs = glob.glob('*.R1.fastq.gz')
+            fastqs = glob.glob('*.R1_001.fastq.gz')
             for fastq1 in fastqs:
                 elements = fastq1.split('.')
                 sample_id = elements[0]
-                fastq2 = f"{sample_id}.R2.fastq.gz"
+                fastq2 = f"{sample_id}.R2_001.fastq.gz"
 
-                sys.stdout.write(f"Renaming {fastq1} to {sample_id}_SN.R1_001.fastq.gz\n")
-                os.rename(fastq1, f"{sample_id}_SN.R1_001.fastq.gz")
+                sys.stdout.write(f"Renaming {fastq1} to {sample_id}_R1_001.fastq.gz\n")
+                os.rename(fastq1, f"{sample_id}_R1_001.fastq.gz")
 
-                sys.stdout.write(f"Renaming {fastq2} to {sample_id}_SN.R2_001.fastq.gz\n")
-                os.rename(fastq2, f"{sample_id}_SN.R2_001.fastq.gz")
+                sys.stdout.write(f"Renaming {fastq2} to {sample_id}_R2_001.fastq.gz\n")
+                os.rename(fastq2, f"{sample_id}_R2_001.fastq.gz")
